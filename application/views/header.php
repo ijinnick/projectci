a5495096613f0c807f2da6764081c65611ae8b9d
Nicz <ndedicatoria@offsourcing.com>
2015-11-05 18:17:39 +0800

    phpstorm

--- application/views/header.php
+++ application/views/header.php
@@ -7,7 +7,7 @@
 
     <meta name="description" content="">
 
-    <title>CodeIgniter Bootstrap</title>
+    <title>edit from phpstorm</title>
 
     <!-- Bootstrap core CSS -->
     <link href="<?php echo base_url('assets/shield/css/bootstrap.css') ?>" rel="stylesheet">
