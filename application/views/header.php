993f0a2909e0d28d78fc8722555acf52884b0d89
Nicz <ndedicatoria@offsourcing.com>
2015-11-05 15:18:58 +0800

    first commit
    app files and folders

---
+++ application/views/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" context="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <meta name="description" content="">
+
+    <title>CodeIgniter Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url('assets/shield/css/bootstrap.css') ?>" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url('assets/shield/css/main.css') ?>" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/icomoon.css') ?>">
+    <link href="<?php echo base_url('assets/shield/css/animate-custom.css') ?>" rel="stylesheet">
+
+
+    
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+    
+    <script src="<?php echo base_url('assets/shield/js/jquery.min.js') ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/shield/js/modernizr.custom.js') ?>"></script>
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="<?php echo base_url('assets/shield/js/html5shiv.js') ?>"></script>
+      <script src="<?php echo base_url('assets/shield/js/respond.min.js') ?>"></script>
+    <![endif]-->
+</head>
+<body>
